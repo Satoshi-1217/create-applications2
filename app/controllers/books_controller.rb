0ce1fd27e5229a7ec2d7cfc0c852c09f782c9018
EC2 Default User <141564706+Satoshi-1217@users.noreply.github.com>
2023-09-13 02:00:55 +0000

    [Update]課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,15 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   
   def index
     @books = Book.all
+    @book = Book.new
     @user = current_user
   end
   
-  def new
-    @book = Book.new
-  end
+  # def new
+  #   @book = Book.new
+  # end
 
   def create
     @book = Book.new(book_params)
@@ -51,6 +53,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
